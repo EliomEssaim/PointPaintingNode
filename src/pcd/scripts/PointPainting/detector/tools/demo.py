425fc9abc5562feb12223aab58249defb3109a2b
linke5 <863040538@qq.com>
2022-09-22 08:22:47 -0400

    add my_demo.sh

--- src/pcd/scripts/PointPainting/detector/tools/demo.py
+++ src/pcd/scripts/PointPainting/detector/tools/demo.py
@@ -94,6 +94,7 @@ def main():
                 points=data_dict['points'][:, 1:], ref_boxes=pred_dicts[0]['pred_boxes'],
                 ref_scores=pred_dicts[0]['pred_scores'], ref_labels=pred_dicts[0]['pred_labels']
             )
+            # mlab.savefig("test.png")
             mlab.show(stop=True)
 
     logger.info('Demo done.')
---
+++ src/pcd/scripts/PointPainting/detector/tools/my_demo.sh
@@ -0,0 +1,1 @@
+python demo.py --cfg_file cfgs/kitti_models/pointpillar_painted.yaml --ckpt ../output/kitti_models/pointpillar_painted/default/ckpt/checkpoint_epoch_80.pth --data_path ../data/kitti/training/painted_lidar/
